fb7dd200e489bcdc130fbc43617492afab76b73a
Dhairya <dhairiya96@gmail.com>
2023-10-06 12:04:45 +0000

    Merge pull request #31 from CrystallizeAPI/next-error-fix

--- application/next.config.js
+++ application/next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/en',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
